1a7bedc97ccf7e9b1965343b025da387536c4a30
qdyyys <qdyyys@gmail.com>
2024-06-20 20:38:20 +0300

    authfix

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
-import { Route, Routes } from "react-router-dom";
+import { Link, Route, Routes } from "react-router-dom";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 function App() {
   return (
     <>
+      <div className="absolute left-1/2 -translate-x-1/2 flex gap-5 text-2xl z-10">
+        <a href="/login">LOGIN</a>
+        <a href="/register">REG</a>
+      </div>
       <Routes>
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
